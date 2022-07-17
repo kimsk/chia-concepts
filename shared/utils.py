7bee74dd4a648c62f9d308892e0d4b0302370594
Karlkim Suwanmongkol <contact@karlk.im>
2022-07-17 14:17:38 -0500

    Singleton Counter (#7)

---
+++ shared/utils.py
@@ -0,0 +1,37 @@
+import json
+from pathlib import Path
+
+from chia.types.blockchain_format.program import Program
+from clvm.casts import int_to_bytes
+from clvm_tools.binutils import disassemble
+from clvm_tools.clvmc import compile_clvm_text
+
+def load_program(file_path, search_paths):
+    clsp = Path(file_path).read_text()
+    return Program(
+        compile_clvm_text(clsp, search_paths)
+    )
+
+def print_program(program):
+    print(disassemble(program))
+
+def print_puzzle(puzzle, tail=0):
+    p = disassemble(puzzle)
+    if tail == 0:
+        print(p)
+    else:
+        print(f'...{p[(tail * -1):]}')
+
+def print_push_tx_result(result):
+    print("additions:")
+    print("==========")
+    for addition in result["additions"]:
+        print(addition)
+        
+    print("removals:")
+    print("=========")
+    for removal in result["removals"]:
+        print(removal)
+
+def print_json(dict):
+    print(json.dumps(dict, sort_keys=True, indent=4))
