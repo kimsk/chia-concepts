96ee34b20a010d15f0dd3fde5d3549e77691c7cc
Karlkim Suwanmongkol <karlkim@gmail.com>
2023-01-09 19:02:40 -0600

    with_rpc_client decorators

--- shared/decorators.py
+++ shared/decorators.py
@@ -2,6 +2,7 @@ import aiosqlite
 from pathlib import Path
 
 from chia.rpc.full_node_rpc_client import FullNodeRpcClient
+from chia.rpc.wallet_rpc_client import WalletRpcClient
 
 # decorator
 # https://medium.com/opex-analytics/database-connections-in-python-extensible-reusable-and-secure-56ebcf9c67fe
@@ -26,3 +27,43 @@ def with_db_connection(db):
 
         return with_connection
     return _with_db_connection
+
+async def run_rpc(rpc_client, f, *args, **kwargs):
+    try:
+        result = await f(rpc_client, *args, **kwargs)
+    except Exception:
+        raise
+    finally:
+        rpc_client.close()
+        await rpc_client.await_closed()
+    return result
+
+def with_full_node_rpc_client(
+        self_hostname,
+        rpc_port,
+        chia_root,
+        chia_config):
+    def _with_full_node_rpc_client(f):
+        async def with_rpc_client(*args, **kwargs):
+            rpc_client = await FullNodeRpcClient.create(
+                self_hostname, rpc_port, chia_root, chia_config
+            )
+            return await run_rpc(rpc_client, f, *args, **kwargs)
+
+        return with_rpc_client
+    return _with_full_node_rpc_client
+
+def with_wallet_rpc_client(
+        self_hostname,
+        rpc_port,
+        chia_root,
+        chia_config):
+    def _with_wallet_rpc_client(f):
+        async def with_rpc_client(*args, **kwargs):
+            rpc_client = await WalletRpcClient.create(
+                self_hostname, rpc_port, chia_root, chia_config
+            )
+            return await run_rpc(rpc_client, f, *args, **kwargs)
+
+        return with_rpc_client
+    return _with_wallet_rpc_client
