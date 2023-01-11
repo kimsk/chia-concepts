e1a7f0c0f3f0a3787923a4973647b7359cf4d1b2
Karlkim Suwanmongkol <karlkim@gmail.com>
2023-01-11 11:52:24 -0600

    single issuance cat spend bundle

--- shared/utils.py
+++ shared/utils.py
@@ -4,7 +4,9 @@ from pathlib import Path
 from rich.console import Console
 from rich.syntax import Syntax
 
+from chia.consensus.default_constants import DEFAULT_CONSTANTS
 from chia.types.blockchain_format.program import Program
+from chia.util.condition_tools import conditions_for_solution
 from clvm.casts import int_to_bytes
 from clvm_tools.binutils import disassemble
 from clvm_tools.clvmc import compile_clvm_text
@@ -15,6 +17,10 @@ def load_program(file_path, search_paths):
     return Program(
         compile_clvm_text(clsp, search_paths)
     )
+
+def load_hex(file_path, search_paths):
+    hex_str = Path(file_path).read_text()
+    return Program.fromhex(hex_str)
 
 def print_clsp(clsp: str, line_numbers=True):
     console = Console()
@@ -33,6 +39,33 @@ def print_puzzle(puzzle, tail=0):
         print(p)
     else:
         print(f'...{p[(tail * -1):]}')
+
+def print_conditions(puzzle, solution):
+    error, conditions, cost = conditions_for_solution(
+            puzzle, 
+            solution, 
+            DEFAULT_CONSTANTS.MAX_BLOCK_COST_CLVM
+    )
+    def to_str(c_vars):
+        return disassemble(Program.to(c_vars))
+
+    console = Console()
+    if error is None:
+        for c in conditions:
+            if len(c.vars) == 1:
+                console.print(Syntax(
+                    f"({c.opcode.name} {to_str(c.vars[0])})", 
+                    "clojure", 
+                    word_wrap=True
+                ))
+            if len(c.vars) == 2:
+                console.print(Syntax(
+                    f"({c.opcode.name} {to_str(c.vars[0])} {to_str(c.vars[1])})",
+                    "clojure",
+                    word_wrap=True,
+                ))
+    else:
+        console.print(f"[bold red]{error}")
 
 def print_push_tx_result(result):
     print("additions:")
